a4daf3279f45b829d638d821fab565fa5a94be20
Ashutosh Paliwal <112794799+ashutoshpaliwal26@users.noreply.github.com>
2024-11-27 11:04:59 +0530

    Update apiContext.ts

--- frontend/src/utils/apiContext.ts
+++ frontend/src/utils/apiContext.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const apiContext = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://virtual-office.onrender.com',
     headers: {
         'Content-Type': 'application/json'
     }
